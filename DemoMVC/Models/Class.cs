19601e86d4b4155e8c607e517082558218227973
Johan Smarius <jsmarius@hotmail.com>
2019-08-18 23:04:36 +0200

    Initial code

---
+++ DemoMVC/Models/Class.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoMVC.Models
+{
+    public class Class
+    {
+        public List<Student> Students { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ DemoMVC/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DemoMVC.Models
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Number { get; set; }
+
+        public int StartYear { get; set; }
+    }
+}
