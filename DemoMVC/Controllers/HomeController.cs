407dabc898349dd8cc301073b87d4daec995a001
Johan Smarius <jsmarius@hotmail.com>
2019-08-20 19:09:08 +0200

    Demo 2

--- DemoMVC/Controllers/HomeController.cs
+++ DemoMVC/Controllers/HomeController.cs
@@ -10,21 +10,9 @@ namespace DemoMVC.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public string Index()
         {
-            return View();
+            return "Hello World";
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
     }
 }
