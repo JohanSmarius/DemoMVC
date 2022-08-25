b938d0d9eb62007cb94acabab5d9406250d8d25d
Johan Smarius <jsmarius@hotmail.com>
2022-08-25 14:59:45 +0200

    Migration

--- DemoMVC/Program.cs
+++ DemoMVC/Program.cs
@@ -1,6 +1,14 @@
-﻿if (app.Environment.IsDevelopment())
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
 {
-    app.UseMigrationsEndPoint();
 }
 else
 {
@@ -15,14 +23,10 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthentication();
-app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.MapControllerRoute(
-    name: "game",
-    pattern: "{controller=Game}/{action=List}/{team?}");
-
 app.Run();
+
